556dcd5b66894a8dbb37bddbe8446742c494b48a
Тим Бад <timurbadalbaev19@gmail.com>
2022-02-24 23:00:59 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+// function check(str, bracketsConfig) {
+  
+  let OPEN_BRACKETS = [];
+  let BRACKETS_PAIR = {};
+
+  bracketsConfig.forEach((item, index) => {
+    OPEN_BRACKETS[index] = item[0];
+    BRACKETS_PAIR[item[1]] = item[0];
+  })
+
+  console.log(OPEN_BRACKETS);
+  console.log(BRACKETS_PAIR);
+
+  function isBracketsOk(str) {
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+
+      let topElement = stack[stack.length - 1];
+
+      if (stack.length > 0 
+        && Object.keys(BRACKETS_PAIR).includes(currentSymbol) 
+        && BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+      } else if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        
+
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
+  } 
+
+  // console.log(46);
+  return isBracketsOk(str);
 }
+
+const config5 = [['(', ')'], ['|', '|']];
+
+// check('|()|(||)||', config5)
